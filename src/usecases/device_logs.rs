797b334ae5f2e9829a8a59c9929b5b44a8d6b807
chansuke <chansuke@georepublic.de>
2021-02-24 14:54:14 +0900

    Fix wrong order of db insertion and error handling (#9)

--- src/usecases/device_logs.rs
+++ src/usecases/device_logs.rs
@@ -2,18 +2,18 @@ use actix_web::{web, HttpResponse};
 use anyhow::Result;
 
 use crate::db::config::Pool;
-use crate::errors::BleScannerError;
+use crate::errors::BleScannerApiError;
 use crate::models::device_logs::DeviceLogs;
 use crate::repositories::device_logs::get_device_logs as list;
 use crate::repositories::device_logs::post_device_logs as post;
 
-pub async fn get_device_logs(db: web::Data<Pool>) -> Result<HttpResponse, BleScannerError> {
+pub async fn get_device_logs(db: web::Data<Pool>) -> Result<HttpResponse, BleScannerApiError> {
     list(db).await
 }
 
 pub async fn post_device_logs(
     db: web::Data<Pool>,
     payloads: web::Json<DeviceLogs>,
-) -> Result<HttpResponse, BleScannerError> {
+) -> Result<HttpResponse, BleScannerApiError> {
     post(db, payloads).await
 }
