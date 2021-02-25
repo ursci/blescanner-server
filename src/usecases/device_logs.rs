2d6c0d6327d9dcd89fd8d138dc8245bdff5914ed
chansuke <chansuke@georepublic.de>
2021-02-25 16:53:14 +0900

    Refactor architecture (#10)
    * Refactor architecture
    
    * Reformat

--- src/usecases/device_logs.rs
+++ src/usecases/device_logs.rs
@@ -1,19 +1,46 @@
 use actix_web::{web, HttpResponse};
 use anyhow::Result;
+use async_trait::async_trait;
 
 use crate::db::config::Pool;
 use crate::errors::BleScannerApiError;
 use crate::models::device_logs::DeviceLogs;
-use crate::repositories::device_logs::get_device_logs as list;
-use crate::repositories::device_logs::post_device_logs as post;
+use crate::repositories::device_logs::{DbContext, HaveDeviceLogRepository, IsDeviceLogRepository};
 
-pub async fn get_device_logs(db: web::Data<Pool>) -> Result<HttpResponse, BleScannerApiError> {
-    list(db).await
+#[async_trait(?Send)]
+pub trait IsDeviceLogUseCase: HaveDeviceLogRepository {
+    async fn get_device_logs(
+        &self,
+        db: web::Data<Pool>,
+    ) -> Result<HttpResponse, BleScannerApiError> {
+        let repository = self.provide_device_logs_repository();
+        let result = repository.get_device_logs(db).await;
+        result
+    }
+
+    async fn post_device_logs(
+        &self,
+        db: web::Data<Pool>,
+        payloads: web::Json<DeviceLogs>,
+    ) -> Result<HttpResponse, BleScannerApiError> {
+        let repository = self.provide_device_logs_repository();
+        let result = repository.post_device_logs(db, payloads).await;
+        result
+    }
 }
 
-pub async fn post_device_logs(
-    db: web::Data<Pool>,
-    payloads: web::Json<DeviceLogs>,
-) -> Result<HttpResponse, BleScannerApiError> {
-    post(db, payloads).await
+pub trait HaveDeviceLogUseCase {
+    type DeviceLogUseCase: IsDeviceLogUseCase;
+
+    fn provide_devicelog_usecase(&self) -> &Self::DeviceLogUseCase;
+}
+
+impl<T: HaveDeviceLogRepository> IsDeviceLogUseCase for T {}
+
+impl HaveDeviceLogUseCase for DbContext {
+    type DeviceLogUseCase = DbContext;
+
+    fn provide_devicelog_usecase(&self) -> &Self::DeviceLogUseCase {
+        &self
+    }
 }
