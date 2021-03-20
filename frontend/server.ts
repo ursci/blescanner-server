245c6bd9804d7886d8791dedc5da26e4e084dd0b
chansuke <chansuke@georepublic.de>
2021-03-20 11:45:14 +0900

    Fix proxy server error

--- frontend/server.ts
+++ frontend/server.ts
@@ -1,33 +1,32 @@
-const express = require('express');
-const next = require('next');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require("express");
+const next = require("next");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const port = 3000
-const dev = process.env.NODE_ENV !== 'production'
+const port = 3000;
+const dev = process.env.NODE_ENV !== "production";
 // should be name of the docker container
-const API_URL = 'http://blescannerbackend:8080/api/v1/device_logs'
+const API_URL = "http://blescannerbackend:8080/api/v1/device_logs";
 
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
   server.use(
-    '/device_logs',
+    "/device_logs",
     createProxyMiddleware({
       target: API_URL,
-      pathRewrite: { [`^/device_logs`]: '' },
-      changeOrigin: true
+      pathRewrite: { [`^/device_logs`]: "" },
+      changeOrigin: true,
     })
   );
 
-  server.all('*', (req: Express.Request, res: Express.Response) => {
-    return handle(req, res)
+  server.all("*", (req, res) => {
+    return handle(req, res);
   });
 
-  server.listen(port, (err: Error) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
